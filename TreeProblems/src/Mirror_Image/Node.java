08ebf32d665f832469a3826559219d0c1bc93a0d
acer <acer@10.203.159.4>
2018-06-06 11:02:19 -0500

    tree Problems

---
+++ TreeProblems/src/Mirror_Image/Node.java
@@ -0,0 +1,53 @@
+package Mirror_Image;
+
+import Tree.BinaryTreeNode;
+
+public class Node implements BinaryTreeNode {
+
+	
+	
+	
+	
+	@Override
+	public void setLeft(BinaryTreeNode node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BinaryTreeNode getLeft() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRight(BinaryTreeNode node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BinaryTreeNode getRight() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BinaryTreeNode clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEqual(BinaryTreeNode node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLessThan(BinaryTreeNode node) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
